d4e3e7a9c1191b46340181c62ae35dcbaee69893
Risshub <rishavsubedi5@gmail.com>
2024-12-05 23:34:02 +0545

    Create New Invoice using Server action in Form, inserting data in DB and rendering in the table

---
+++ nextjs-dashboard/app/lib/actions.ts
@@ -0,0 +1,43 @@
+'use server';
+
+import {z} from 'zod';  // using a type validation library
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache'; 
+import { redirect } from 'next/navigation';
+
+// defining a schema that matches the shape of the form object to validate the formData before saving it to the database.
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+
+// amount field is specifically set to coerce (change) from a string to a number
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+ 
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const {customerId, amount, status} = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  //storing monetary values in cents in the database to eliminate JavaScript floating-point errors
+  const amountInCents = amount * 100;
+
+  //creating a new date with the format "YYYY-MM-DD" for the invoice's creation date
+  const date = new Date().toISOString().split('T')[0];
+
+  // Inserting the data into the database
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  // Revalidating the path to ensure the newly created invoice is reflected in the cache and redirecting to InvoicesPage
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
