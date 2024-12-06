6c19b87df5a4716a522197669c485b79c7dd94da
Risshub <rishavsubedi5@gmail.com>
2024-12-06 15:21:06 +0545

    server action for deleting an invoice

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -62,3 +62,9 @@ export async function updateInvoice(id: string, formData: FormData) {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
+
+// **Server Action for deleting an invoice**
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
